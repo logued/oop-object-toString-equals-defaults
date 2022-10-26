00997d713419d3fd322faf31aabdc276bd57d53e
dermo <dermot.logue@dkit.ie>
2022-10-26 16:42:26 +0100

    code added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,24 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Object superclass - default methods");
+
+        Position p1 = new Position(10,20);
+
+        System.out.println("Position p1 = " + p1.toString());
+
+        Position p2 = new Position(10,20);
+
+        if( p1.equals(p2))
+            System.out.println("p1 is equal to p2");
+        else
+            System.out.println("p1 and p2 are not equal");
+
+        if( p1.equals(p1))
+            System.out.println("p1 is equal to p1");
+        else
+            System.out.println("p1 and p1 are not equal");
+
     }
 }
--- src/main/java/org/example/Position.java
+++ src/main/java/org/example/Position.java
@@ -1,2 +1,23 @@
-package org.example;public class Position {
+package org.example;
+
+// Has this class got a Superclass?
+// YES, th eObject class is the superclass of all classes,
+// so this class inherits from the Object class.
+
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
